b0323dbab495d4eb24e1c45cb50648d5b576ab7b
Michael Humphries <michaelhumphries1997@gmail.com>
2022-03-30 15:53:23 +0000

    adding stuff

--- hue-utils-ui/src/App.js
+++ hue-utils-ui/src/App.js
@@ -1,16 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ListView from "./components/list-view";
+import MenuBar from "./components/menu-bar";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
-  console.log("here 1");
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <body className="App-body">
-        <ListView />
-      </body>
-    </div>
+      <Router>
+        <div className="App">
+          <header className="App-header"></header>
+          <MenuBar />
+        </div>
+          <Routes>
+            <Route path="/about">
+              {"foo"}
+            </Route>
+            <Route path="/users">
+              {"foo"}
+            </Route>
+            <Route path="/">
+              {"foo"}
+            </Route>
+          </Routes>
+      </Router>
   );
 }
 
